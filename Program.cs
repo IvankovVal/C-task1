851f365df8700917bdc0aa823a5eabb8fca9bc3f
Валерий Юрьевич <v.ivankov@dunice.net>
2023-03-30 14:34:39 +0300

    optimize code by concatination and min/max value

--- Program.cs
+++ Program.cs
@@ -1,26 +1,18 @@
-﻿//Логические переменные
+﻿string minVal= " Наименьшее значение : ";
+string maxVal= " Наибольшее значение : ";
+//Логические переменные
 bool a = true;
 Console.WriteLine($"I think it is {a}");
 //byte
-byte b, c;
-b = 0; c = 255;
-Console.WriteLine($"{b} - наименьший из byte, а {c} - наибольшее.");
+Console.WriteLine("Тип byte." + minVal + byte.MinValue + maxVal + byte.MaxValue );
 //int
-int d, e;
-d = -2147483648; e = 2147483647;
-Console.WriteLine($"{d} - наименьший из int, а {e} - наибольшее.");
+Console.WriteLine("Тип int." + minVal + int.MinValue + maxVal + int.MaxValue );
 //long
-long f, g;
-f = -9223372036854775808; g = 9223372036854775807;
-Console.WriteLine($"{f} - наименьший из long, а {g} - наибольшее.");
+Console.WriteLine("Тип long." + minVal + long.MinValue + maxVal + long.MaxValue );
 //float
-float h, i;
-h = 3.4e38f; i = -3.4e38f;
-Console.WriteLine($"{h} - наименьший из float, а {i} - наибольшее.");
+Console.WriteLine("Тип float." + minVal + float.MinValue + maxVal + float.MaxValue );
 //double
-double j, k;
-j = 5e-324; k = 1.7e+308;
-Console.WriteLine($"{j} - наименьший из double, а {k} - наибольшее.");
+Console.WriteLine("Тип double." + minVal + double.MinValue + maxVal + double.MaxValue );
 //char
 char l, m;
 l ='к'; m = 'у';
