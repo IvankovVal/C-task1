9b64388df34a653c20b45cb7fe2691b5e60c8f46
Валерий Юрьевич <v.ivankov@dunice.net>
2023-04-03 18:12:38 +0300

    проверяем тип введённых чисел

--- Program.cs
+++ Program.cs
@@ -2,24 +2,51 @@
  и показывать какое больше, а также выводить их сумму.
 */
 
+int int1 = 0;
+int int2 = 0;
+
+double double1;
+double double2;
+
+//Первое число
 Console.WriteLine("Введите первое число");
 string? str1 = "";
 str1 = Console.ReadLine();
+bool isInt = int.TryParse(str1, out int1);
+if (isInt) {
+    int1 = int.Parse(str1);
+    Console.WriteLine("Тип числа - int.");
 
+} 
+else{
+//Проверить на возможность 
+bool isDouble = double.TryParse(str1, out double1);
+if (isDouble) {
+    double1 = double.Parse(str1);
+    Console.WriteLine("Тип числа - double.");
+
+}
+
+else Console.WriteLine("Не верный формат числа");
+}
+
+
+
+
+
+//Второе число
 Console.WriteLine("Введите второе число");
 string? str2 = "";
 str2 = Console.ReadLine();
 
-int num1 = 0;
-int num2 = 0;
 
-num1 = int.Parse(str1);
-num2 = int.Parse(str2);
 
-if(num1 > num2){
-Console.WriteLine($"{num1} больше, чем {num2}");
+int2 = int.Parse(str2);
+
+if(int1 > int2){
+Console.WriteLine($"{int1} больше, чем {int2}");
 }
 else { 
-Console.WriteLine($"{num2} больше, чем {num1}");
+Console.WriteLine($"{int2} больше, чем {int1}");
 }
-Console.WriteLine($"Сумма чисел равна: а{num2 + num1}");
+Console.WriteLine($"Сумма чисел равна: а{int2 + int1}");
