23b31fd07b8906b528df69e86721e9f507679a97
Валерий Юрьевич <v.ivankov@dunice.net>
2023-04-04 19:28:50 +0300

    Работатет с int

--- Program.cs
+++ Program.cs
@@ -1,52 +1,20 @@
-﻿/*Создать консольное приложение, которое будет принимать 2 любых числа одного типа (int, float, double)
- и показывать какое больше, а также выводить их сумму.
-*/
+﻿string? str1;
+string? str2;
 
-int int1 = 0;
-int int2 = 0;
-
-double double1;
-double double2;
-
-//Первое число
-Console.WriteLine("Введите первое число");
-string? str1 = "";
+Console.WriteLine("Введите число типа int");
 str1 = Console.ReadLine();
-bool isInt = int.TryParse(str1, out int1);
-if (isInt) {
-    int1 = int.Parse(str1);
-    Console.WriteLine("Тип числа - int.");
-
-} 
-else{
-//Проверить на возможность 
-bool isDouble = double.TryParse(str1, out double1);
-if (isDouble) {
-    double1 = double.Parse(str1);
-    Console.WriteLine("Тип числа - double.");
-
-}
-
-else Console.WriteLine("Не верный формат числа");
-}
-
-
-
-
-
-//Второе число
-Console.WriteLine("Введите второе число");
-string? str2 = "";
+Console.WriteLine("Введите второе число типа int");
 str2 = Console.ReadLine();
 
+int num1Int;
+bool is1numInt = int.TryParse(str1, out num1Int);
+int num2Int;
+bool is2numInt = int.TryParse(str2, out num2Int);
 
-
-int2 = int.Parse(str2);
-
-if(int1 > int2){
-Console.WriteLine($"{int1} больше, чем {int2}");
+if(!is1numInt) Console.WriteLine("Не верный ввод первого числа");
+if(!is2numInt) Console.WriteLine("Не верный ввод второго числа");
+if(is1numInt & is2numInt){
+    if(num1Int > num2Int) Console.WriteLine($"{num1Int} больше, чем {num2Int}");
+    else Console.WriteLine($"{num2Int} больше, чем {num1Int}");
+    Console.WriteLine($"{num1Int + num2Int}");
 }
-else { 
-Console.WriteLine($"{int2} больше, чем {int1}");
-}
-Console.WriteLine($"Сумма чисел равна: а{int2 + int1}");
