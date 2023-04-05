1da5e14a6f1a33ef8f8ffcbf6d2ac63e05fd3237
Валерий Юрьевич <v.ivankov@dunice.net>
2023-04-05 18:19:43 +0300

    С вводом границ с консоли

--- Program.cs
+++ Program.cs
@@ -1,8 +1,27 @@
 ﻿/*Создать консольное приложение, в котором можно задать минимальное и максимальное число (тип int),
  после чего в консоль выводятся все простые числа в указанном промежутке.
 */
+Console.WriteLine("Введите минимальное число");
+string? str1;
+str1 = Console.ReadLine();
+int num1 = 0;
+if(str1 != null) {
+num1 = int.Parse(str1);
+}
+else Console.WriteLine("Вы ничего не ввели, поэтому будет использован 0.");
+
+
+Console.WriteLine("Введите максимальное число");
+string? str2;
+str2 = Console.ReadLine();
+int num2 = 0;
+if(str2 != null) {
+num2 = int.Parse(str2);
+}
+else Console.WriteLine("Вы ничего не ввели, поэтому будет использован 0.");
+
 //Вызов функции
-MakeInterval(5,7);
+MakeInterval(num1,num2);
 
 //Функция
 void MakeInterval(int first, int second){
